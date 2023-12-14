e808cdbbfb8c83d26e647db08cc386af6d6ecff6
webdevzsk <72684302+webdevsk@users.noreply.github.com>
2023-12-14 21:03:09 +0600

    Random fact section done

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 // Animating scroll of scrollable containers
-$(document).ready(function () {
-    $('.scrollable').hover(function () {
-        var scrollHeight = $(this)[0].scrollHeight
-        $(this).animate({
-            scrollTop: scrollHeight
-        }, { duration: 10000 })
-    }, function () {
-        $(this).animate({
-            scrollTop: 0
-        }, { duration: 500 })
-    })
+$('.scrollable').hover(function () {
+    const scrollHeight = $(this)[0].scrollHeight
+    $(this).animate({ scrollTop: scrollHeight }, { duration: 10000 })
+}, function () {
+    $(this).animate({ scrollTop: 0 }, { duration: 500 })
+})
+
+
+// Task 1 Ajax Call
+$('#random-fact').html('<div class="spinner"></div>')
+$.ajax({
+    url: 'http://numbersapi.com/1/30/date?json',
+    success: function (data) {
+        $('#random-fact').html(`<p class="fs-4">${data.text}</p>`)
+    },
+    error: function (error) {
+        $('#random-fact').html(`An error occurred while fetching data. ${error?.message}`)
+    }
 })
